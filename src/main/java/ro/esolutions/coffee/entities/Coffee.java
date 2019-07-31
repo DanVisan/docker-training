1f505493d73f9c9bf74f56d38438ef68757e1964
dan <dan.visan@esolutions.ro>
2019-07-31 15:09:22 +0300

    Add end to end test

--- src/main/java/ro/esolutions/coffee/entities/Coffee.java
+++ src/main/java/ro/esolutions/coffee/entities/Coffee.java
@@ -2,6 +2,7 @@ package ro.esolutions.coffee.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Coffee {
@@ -23,5 +24,27 @@ public class Coffee {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public Coffee() {
+	}
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Coffee coffee = (Coffee) o;
+		return Objects.equals(id, coffee.id) &&
+				Objects.equals(name, coffee.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
 	}
 }
--- src/test/java/ro/esolutions/coffee/CoffeeApplicationTests.java
+++ src/test/java/ro/esolutions/coffee/CoffeeApplicationTests.java
@@ -3,12 +3,29 @@ package ro.esolutions.coffee;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+import ro.esolutions.coffee.entities.Coffee;
+
+import static org.junit.Assert.assertEquals;
 
 public class CoffeeApplicationTests {
 
+	RestTemplate restTemplate = new RestTemplate();
+
 	@Test
-	public void contextLoads() {
+	public void givenId_whenGetCoffeeEndpointIsCalled_ThenReturnCoffee() {
+		//given
+		String id = "1";
+		Coffee expectedCoffee = new Coffee("1", "Espresso");
+
+		//when
+		Coffee actualResult = restTemplate.getForObject("http://localhost:8080/coffee/{id}", Coffee.class, id);
+
+		//then
+		assertEquals(expectedCoffee, actualResult);
+
 	}
 
 }
